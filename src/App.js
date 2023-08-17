b7b17032c46eab2fd5e38b40a4cc540c81125e32
MaiVanBinh <55869650+MaiVanBinh@users.noreply.github.com>
2023-08-17 09:31:44 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Conatainer = styled("div")`
+  .app-header {
+    background-color: aqua;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 80px;
+  }
+
+  .app-content {
+    background-color: bisque;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 500px;
+  }
+
+  .app-footer {
+    background-color: blueviolet;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100px;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Conatainer>
+      <header className="app-header">header</header>
+      <div className="app-content">content</div>
+      <div className="app-footer">Footer</div>
+    </Conatainer>
   );
 }
 
